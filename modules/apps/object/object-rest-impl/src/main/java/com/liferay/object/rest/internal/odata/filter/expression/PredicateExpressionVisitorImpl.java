e88acc13bc9692faa13842c7f74392089918033d
Tai Pham <tai.pham@gs.liferay.com>
2023-02-10 12:31:13 +0700

    LPS-172979 Add static method Predicate.notOf to negate a predicate

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/odata/filter/expression/PredicateExpressionVisitorImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/odata/filter/expression/PredicateExpressionVisitorImpl.java
@@ -325,7 +325,7 @@ public class PredicateExpressionVisitorImpl
 				defaultPredicate.getRightExpression());
 		}
 
-		return Predicate.not(defaultPredicate);
+		return Predicate.notOf(defaultPredicate);
 	}
 
 	private PredicateExpressionVisitorImpl(
